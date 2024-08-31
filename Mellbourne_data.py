e64677c4f2ba24fad3465852ece3ec598b4d50b9
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2024-09-01 00:28:44 +0300

    Update Mellbourne_data.py

--- Mellbourne_data.py
+++ Mellbourne_data.py
@@ -1,22 +1,26 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+
 
 df = pd.read_csv("melb_data.csv")
 #               na = not available
 melbourne_data = df.dropna(axis=0)
 
+
 y = melbourne_data.Price
-
-melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 
+                      'YearBuilt', 'Lattitude', 'Longtitude']
 X = melbourne_data[melbourne_features]
-
-
-# print(melbourne_data.describe())
-
 #                                   same results each run
 melbourne_model = DecisionTreeRegressor(random_state=1)
-
 melbourne_model.fit(X, y)
+
+
+predicted_home_prices = melbourne_model.predict(X)
+mean_absolute_error(y, predicted_home_prices)
+
 
 print(X.describe())
 print("Making predictions for the following 5 houses:")
