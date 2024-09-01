545c938847826527f90430278beb4f1154541e88
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2024-09-01 23:50:14 +0300

    Update Mellbourne_data.py

--- Mellbourne_data.py
+++ Mellbourne_data.py
@@ -53,3 +53,7 @@ for max_leaf_nodes in [5, 50, 500, 5000]:
 
 
 
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
