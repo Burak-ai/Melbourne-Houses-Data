876bfaa3eaf090af0c3972f30fd9eed56f5a21ab
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2024-09-01 23:50:02 +0300

    Update Mellbourne_data.py

--- Mellbourne_data.py
+++ Mellbourne_data.py
@@ -50,3 +50,6 @@ def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
 for max_leaf_nodes in [5, 50, 500, 5000]:
     my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
     print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+
+
+
